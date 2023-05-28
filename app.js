b2b5530ecfda22e8ee95a38caa4a4264e9b3de20
Facundo <esquivelfacundo.97@gmail.com>
2023-05-28 04:31:21 -0300

    update

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ document.addEventListener( "DOMContentLoaded",()=>{
   
  const form = document.querySelector('.form');
   const input = document.querySelector('.inputNum')
+  const dolar = document.querySelector('.inputDolar')
   const container = document.querySelector('.container')
-  let dolar = document.querySelector('.inputDolar')
   let inputNumber;
   let inputDolar;
   
-  
+  function redondearDecimales(numero, decimales) {
+    numeroRegexp = new RegExp('\\d\\.(\\d){' + decimales + ',}');
+    if (numeroRegexp.test(numero)) {         
+      return Number(numero.toFixed(decimales));
+    } else {
+        return Number(numero.toFixed(decimales)) === 0 ? 0 : numero;
+    }
+}
+
   dolar.addEventListener('change',(e)=>{
     inputDolar = e.target.value;
   })
@@ -16,6 +24,6 @@ document.addEventListener( "DOMContentLoaded",()=>{
   })
   form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    container.textContent = inputNumber && inputDolar ? inputNumber > inputDolar*10 ? `El precio final es: $ ${(inputNumber * 1.53)}. Cuando el valor base del juego supera los 10 dolares el impuesto a sumarle es del 53%.` : `El precio final es: $ ${(inputNumber * 1.74)}. Cuando el valor base del juego no supera los 10 dolares el impuesto a sumarle es de 74%.` : 'ingresa ambos campos';
+    container.textContent = inputNumber && inputDolar ? inputNumber > inputDolar*10 ? `El precio aproximado final es: $ ${redondearDecimales(inputNumber * 1.53,2)}. Cuando el valor base del juego supera los 10 dolares el impuesto a sumarle es del 53%.` : `El precio aproximado final es: $ ${redondearDecimales(inputNumber * 1.74,2)}. Cuando el valor base del juego no supera los 10 dolares el impuesto a sumarle es de 74%.` : 'Completa ambos campos';
   })
 })
